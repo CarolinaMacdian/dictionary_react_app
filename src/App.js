381d9ca2d11cb8c570b9cc69505b89e3f7ef1f48
CarolinaMacdian <115894648+CarolinaMacdian@users.noreply.github.com>
2023-03-10 15:20:06 +0000

    app creation and searchEngine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 /** @format */
 
 import "./App.css";
+import SearchEngine from "./SearchEngine";
 
 export default function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>Hello!</p>
-      </header>
+      <div className='container'>
+        <SearchEngine />
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href='https://linkedin.com/in/carolinamacdian'
+              className='link'
+              target='_blank'
+              rel='noreferrer'>
+              Carolina Macdian
+            </a>
+            , is{" "}
+            <a
+              href='https://github.com/CarolinaMacdian/dictionary_react_app'
+              className='link'
+              target='_blank'
+              rel='noreferrer'>
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href='https://sprightly-starlight-cf8f53.netlify.app/'
+              className='link'
+              target='_blank'
+              rel='noreferrer'>
+              hosted on Netlify
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
