7d63758cecca2503361803a15b44184c39a22454
CarolinaMacdian <115894648+CarolinaMacdian@users.noreply.github.com>
2023-03-10 16:15:42 +0000

    api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,12 +2,21 @@
 
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    const apiKey = "466o652f33a4a9b0t59607ec49888d3f";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
