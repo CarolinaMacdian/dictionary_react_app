904a8a9819bc0140ad2ff40ca327b6c6668f1b83
CarolinaMacdian <115894648+CarolinaMacdian@users.noreply.github.com>
2023-03-17 15:14:14 +0000

    max width alteration

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,7 +9,7 @@ export default function Meaning(props) {
   return (
     <div className='Meaning'>
       <span>{props.meaning.partOfSpeech}</span>
-      <p>{props.meaning.definition}</p>
+      <p> {props.meaning.definition}</p>
       <div className='Example'>{props.meaning.example}</div>
       <Synonyms synonyms={props.meaning.synonyms} />
     </div>
